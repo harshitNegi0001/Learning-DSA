a011102dd7e03316197334320727d1bb3adab293
Harshit Singh Negi <negiharshitsingh919@gmail.com>
2024-08-16 22:51:20 +0530

    Add files via upload

--- stackDSA.java
+++ stackDSA.java
@@ -19,6 +19,9 @@ class Stack{
 	public int size(){
 		return length;
 	}
+	public boolean isEmpty(){
+		return top==null;
+	}
 	public void push(int d){
 		Node current= new Node(d);
 		
@@ -45,6 +48,39 @@ class Stack{
 }
 
 public class StackDSA {
+	public static int[] nextGreaterElement(int[] arr){
+	int[] result=new int[arr.length];
+	Stack obj=new Stack();
+	for(int i=arr.length-1;i>=0;i--){
+		if(!obj.isEmpty()) {
+			while(!obj.isEmpty()&&obj.peek()<=arr[i]){
+				obj.pop();
+			}
+
+		}
+		if(obj.isEmpty()){ 
+			result[i]=-1;
+		} 
+		else {
+			result[ i ] = obj.peek();
+		}
+		obj.push(arr[ i ]);
+	}
+
+return result;
+	}
+	public static int[] reverseArray(int[] arr){
+		//this method use stack to reverse a Array...
+		Stack array = new Stack();
+		for(int i=0;i<arr.length;i++){
+			array.push(arr[i]);
+		}
+		
+		for(int i=0;i<arr.length;i++){
+			arr[i]= array.pop();
+		}
+		return arr;
+	}
 	public static void main(String[] args) {
 		Stack stack = new Stack();
         
@@ -59,5 +95,17 @@ public class StackDSA {
         System.out.println("top element in stack: " + stack.peek());
         System.out.println("Popped element: " + stack.pop());
         
+        int[] arr={1,2,3,4,5};
+        arr=reverseArray(arr);
+        for(int item:arr){
+        	System.out.println(item);
+        }
+        int[] arr2={4,7,3,4,8,1};
+        arr2=nextGreaterElement(arr2);
+        System.out.print("("+arr2[0]);
+        for(int i=1;i<arr2.length;i++){
+        	System.out.print(","+arr2[i]);
+        }
+        System.out.println(")");
 	}
 }
