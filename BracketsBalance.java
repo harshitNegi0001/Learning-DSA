36d6a87d0535a75728430284bf67e7f8f52a02cd
Harshit Singh Negi <negiharshitsingh919@gmail.com>
2024-08-17 20:08:10 +0530

    Add files via upload

---
+++ BracketsBalance.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class StackPractice {
+	public static boolean isBalanced(String s){
+		
+		Stack <Character> stack = new Stack<>();
+		
+		for(int i=0;i<s.length();i++){
+			if((s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{')){
+				stack.push(s.charAt(i));
+			}
+			else{
+				if(stack.isEmpty()){
+					return false;
+				}
+				if((s.charAt(i)==')'&&stack.peek()=='(')||(s.charAt(i)==']'&&stack.peek()=='[')||(s.charAt(i)=='}'&&stack.peek()=='{')){
+					stack.pop();
+				}
+				
+				else{
+					return false;
+				}
+				
+			}
+		}
+		return stack.isEmpty();
+		
+	}
+	public static void main(String[] args) {
+		String s1="()[{}]{([])}";
+		String s2="[)";
+		System.out.println(isBalanced(s1));
+	}
+}
