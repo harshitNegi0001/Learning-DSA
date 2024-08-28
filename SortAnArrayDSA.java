82456625259b71c919b39566010e5bdd9f7fbf55
Harshit Singh Negi <negiharshitsingh919@gmail.com>
2024-08-28 20:38:26 +0530

    Add files via upload

--- SortAnArrayDSA.java
+++ SortAnArrayDSA.java
@@ -17,6 +17,43 @@ public class SortingAlgo {
 		}
 		System.out.println("Sorted ");
 	}
+	public static void insertionSort(int [] arr){
+		if(isSorted(arr)){
+					System.out.println("Already Sorted ");
+					return;
+				}
+		int j,current;
+		for(int i=1; i<arr.length; i++){
+			j=i-1;
+			current=arr[i];
+			while(j>=0 && current<arr[j]){
+				
+				arr[j+1]=arr[j];			
+				j--;
+			}			
+			arr[j+1]=current;
+			
+		}
+		
+	}
+	public static void selectionSort(int [] arr){
+		int min;
+		for(int i=0;i<arr.length-1;i++){
+			min=i;
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[min]>arr[j]){
+					min=j;
+				}
+			}
+			
+			swap(arr,i,min);
+		}
+	}
+	public static void swap(int [] arr,int i1,int i2){
+		int temp= arr[i1];
+		arr[i1]= arr[i2];
+		arr[i2]=temp;
+	}
 	public static void print(int[] arr){
 		for(int item: arr){
 			System.out.print(item+" ");
@@ -31,10 +68,12 @@ public class SortingAlgo {
 		}
 		return true;
 	}
+	
+	
 	public static void main(String[] args) {
-		int [] arr={1,0,5,8,3,10,2,16,14};
+		int [] arr={1,0,5,8,3,10,2,14,16};
 		
-		bubbleSort(arr);
+		selectionSort(arr);
 		for(int item: arr){
 			System.out.print(item+" ");
 		}
