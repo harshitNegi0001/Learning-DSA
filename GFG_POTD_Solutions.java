abe6864c7705a54e8edc3c12f8e82d15f062f4fb
Harshit Singh Negi <negiharshitsingh919@gmail.com>
2024-10-08 21:37:10 +0530

    Add files via upload

---
+++ GFG_POTD_Solutions.java
@@ -0,0 +1,20 @@
+public class SolutionOfPOTD {
+	/*
+	//8_oct_24
+	public static int pairsum(int[] arr) {
+        int num1=Integer.MIN_VALUE;
+        int num2=num1;
+        for(int num : arr){
+            if(num>=num1){
+                num2=num1;
+                num1=num;
+            }
+            else if(num>num2) num2=num;
+        }
+        return num1+num2;
+    }
+    */
+	public static void main(String[] args) {
+		
+	}
+}
